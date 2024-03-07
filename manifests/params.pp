297615573387e2d8818a95ce8a02e6183a53daff
Tyler Sparr <tyler.sparr@encore.tech>
2024-03-07 15:06:47 -0500

    Adding commentary

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,6 +3,8 @@
 # @api private
 #
 class jamf::params {
+  # If you are running anything less than Jamf 11.3, this needs to be set to
+  # 'jamf.tomcat8'
   $tomcat_service = 'jamf.tomcat'
 
   if $facts['os']['family'] == 'Redhat' {
