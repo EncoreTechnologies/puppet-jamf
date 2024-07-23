fd850798a408e7e16811c29fc034fe362fec5f8c
Rick Paxton <rick.paxton@encore.tech>
2024-07-23 11:27:22 -0400

    Check jamf version

--- lib/facter/package_version.rb
+++ lib/facter/package_version.rb
@@ -1,5 +1,9 @@
 Facter.add(:jamf_version) do
-  setcode do
-    Facter::Core::Execution.execute("/usr/local/jss/bin/jamf-pro --version | head -1 | awk '{ print $2 }'")
+  File.open( "/usr/local/jss/tomcat/webapps/ROOT/WEB-INF/xml/version.xml" ).each do |line|
+    if line.include?('webApplicationVersion')
+      setcode do
+        "#{line}".slice!(/>(.*)-/, 1)
+      end
+    end
   end
 end
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@
 class jamf::params {
   # If you are running anything less than Jamf 11.3, this needs to be set to
   # 'jamf.tomcat8'
-  if versioncmp($facts['jamf_version'], '3.0.0') < 0 {
+  if versioncmp($facts['jamf_version'], '11.3.0') < 0 {
     $tomcat_service = 'jamf.tomcat8'
   } else {
     $tomcat_service = 'jamf.tomcat'
